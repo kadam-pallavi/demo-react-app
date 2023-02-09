4330dc3f1b4fc2fb997d5e78d2462ba43d95b0b2
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-02-10 00:24:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+import React, { useState } from "react";
+import '../styles/App.css';
+import Welcome from './Welcome';
+
+const App = () => {
+
+
+  return (
+    <Welcome name="Pallavi" />
+  )
+}
+
+
+export default App;
